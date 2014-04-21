9347b1d90eb7fd83fa2d690efbe77ac62ed8ddca
Raghu Betina <git@rugb.com>
2014-04-21 17:02:03 -0500

    Initial commit

---
+++ yearbook.rb
@@ -0,0 +1,48 @@
+s1 = {
+  "image_url" => "https://www.kellogg.northwestern.edu/images/students/all/x411843.jpg",
+  "first_name" => "Abby",
+  "last_name" => "Kass"
+}
+
+s2 = {
+  "image_url" => "https://www.kellogg.northwestern.edu/images/students/all/x408193.jpg",
+  "first_name" => "Akshat",
+  "last_name" => "Mehrotra"
+}
+
+s3 = {
+  "image_url" => "https://www.kellogg.northwestern.edu/images/students/all/x424158.jpg",
+  "first_name" => "Alexander",
+  "last_name" => "Greenwood"
+}
+
+s4 = {
+  "image_url" => "https://www.kellogg.northwestern.edu/images/students/all/x424285.jpg",
+  "first_name" => "Andrew",
+  "last_name" => "Kim"
+}
+
+students_array = [s1, s2, s3, s4]
+
+# When you run this script, you should see the following output:
+
+# KIEI925 Students
+# ================
+
+# - Abby Kass (https://www.kellogg.northwestern.edu/images/students/all/x411843.jpg)
+# - Akshat Mehrotra (https://www.kellogg.northwestern.edu/images/students/all/x408193.jpg)
+# - Alexander Greenwood (https://www.kellogg.northwestern.edu/images/students/all/x424158.jpg)
+# - Andrew Kim (https://www.kellogg.northwestern.edu/images/students/all/x424285.jpg)
+
+# Use the .each method on the students_array to do your work.
+
+# YOUR CODE GOES BELOW:
+
+
+
+
+
+
+
+
+
